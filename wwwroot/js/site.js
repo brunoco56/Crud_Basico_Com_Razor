9f2322c65a55fe287b00a95fadfc3640482d0da2
Bruno <brunoco56@hotmail.com>
2021-02-06 17:13:36 -0300

    datapicker portugues

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,5 +3,16 @@
 
 // Write your JavaScript code.
 $(function () {
-    $('input[type=datetime]').datepicker({dateFormat: 'dd/mm/yy' });
+    $('input[type=datetime]').datepicker({
+        dateFormat: 'dd/mm/yy',
+        dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+        dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+        monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        nextText: 'Proximo',
+        prevText: 'Anterior'
+    });
 });
+
+
